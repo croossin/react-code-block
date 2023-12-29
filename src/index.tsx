d67b2af4294f0974a878a12f872b8e9ab61eb090
Chase Roossin <croossin94@gmail.com>
2023-12-29 05:26:39 -0800

    Move margin left for line number if the row is meant to be highlighted

--- src/index.tsx
+++ src/index.tsx
@@ -117,7 +117,14 @@ function CodeBlock({
                           )}
                           <div className="px-4">
                             {showLineNumbers && (
-                              <span className="mr-4 select-none">{i + 1}</span>
+                              <span
+                                className={clsx(
+                                  "select-none",
+                                  shouldHighlight ? "-ml-1 mr-5" : "mr-4"
+                                )}
+                              >
+                                {i + 1}
+                              </span>
                             )}
                             {line.map((token, key) => (
                               <span key={key} {...getTokenProps({ token })} />
